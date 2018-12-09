89e6bb23befea9321a4ecff8f0979570728c1a13
Babagana Zannah <babagana.tech@gmail.com>
2018-12-09 18:31:00 +0100

    update project with default and base controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends BaseController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
