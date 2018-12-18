7f101f675e3fb4896c8aa525018f7c7490c3cfcb
Babagana Zannah <babagana.tech@gmail.com>
2018-12-18 10:20:43 +0100

    update fixtures and add userinterface

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,21 +3,61 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoder
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoder $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
         for ($i = 1; $i <=10; $i++) {
+            /** @var User $user */
+            $user = $this->getReference('user_admin_'.random_int(1, 10));
             $blog = new BlogPost();
             $blog->setTitle("In the Dark " . $i)
-                 ->setPublished(new \DateTime("2018-12-31 23:59:59"))
-                 ->setContent("Boring through the hall number " . $i)
-                 ->setAuthor("John Cast " . $i)
-                 ->setSlug("in-the-dark-".$i);
+                ->setPublished(new \DateTime("2018-12-31 23:59:59"))
+                ->setContent("Boring through the hall number " . $i)
+                ->setAuthor($user)
+                ->setSlug("in-the-dark-".$i);
             $manager->persist($blog);
+        }
+
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        for ($i = 1; $i <=10; $i++) {
+            $user = new User();
+            $user->setUsername('admin'.$i)
+                ->setEmail('admin@example.com')
+                ->setName('Foo Bar')
+                ->setPassword($this->passwordEncoder->encodePassword($user, 'foo'));
+            $this->addReference('user_admin_'.$i, $user);
+            $manager->persist($user);
         }
 
         $manager->flush();
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,13 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -125,5 +126,47 @@ class User
     public function getComments(): ArrayCollection
     {
         return $this->comments;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return array('ROLE_USER');
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
