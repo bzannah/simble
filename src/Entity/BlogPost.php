27e41d69baaf6be3743a9633daf12dec4a3f0341
Babagana Zannah <babagana.tech@gmail.com>
2019-04-02 16:51:10 +0200

    Add validation and access controller on blog POST

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -6,12 +6,13 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
  * @ApiResource(
  *     itemOperations={"get"},
- *     collectionOperations={"get"}
+ *     collectionOperations={"get", "post" = {"access_control" = "is_granted('IS_AUTHENTICATED_FULLY')"}}
  * )
  */
 class BlogPost
@@ -25,16 +26,20 @@ class BlogPost
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $title;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
      */
     private $published;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
      */
     private $content;
 
@@ -48,6 +53,7 @@ class BlogPost
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $slug;
 
