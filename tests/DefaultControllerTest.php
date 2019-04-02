f8c49cadff2b7ea1f784866ee76769168ffe16a2
Babagana Zannah <babagana.tech@gmail.com>
2019-04-02 14:50:04 +0200

    Add TravisCI intergration and Unit Test intergration

---
+++ tests/DefaultControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class DefaultControllerTest extends TestCase
+{
+
+    public function testDefault()
+    {
+        $this->assertEquals(true, true);
+    }
+}
