56672b25903edb9018c141fdfca7669c4685a249
Babagana Zannah <babagana.tech@gmail.com>
2019-04-03 10:33:26 +0200

    Add eventSubscriber to set current User as Post Author

---
+++ src/EventSubscriber/AuthoredEntitySubscriber.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\BlogPost;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AuthoredEntitySubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * AuthoredEntitySubscriber constructor.
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['getAuthenticatedUser', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    /**
+     * @param GetResponseForControllerResultEvent $event
+     */
+    public function getAuthenticatedUser(GetResponseForControllerResultEvent $event)
+    {
+        $entity = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        /** @var UserInterface $author */
+        $author = $this->tokenStorage->getToken()->getUser();
+
+        if (!$entity instanceof BlogPost || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $entity->setAuthor($author);
+    }
+}
