ce73f23522185be02dae6d9ab70e51e33398493a
Babagana Zannah <babagana.tech@gmail.com>
2019-04-08 16:32:20 +0200

    Modify resource entity with auto author assignment

--- src/EventSubscriber/AuthoredEntitySubscriber.php
+++ src/EventSubscriber/AuthoredEntitySubscriber.php
@@ -6,6 +6,7 @@ namespace App\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\BlogPost;
+use App\Entity\Comment;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
@@ -50,7 +51,7 @@ class AuthoredEntitySubscriber implements EventSubscriberInterface
         /** @var UserInterface $author */
         $author = $this->tokenStorage->getToken()->getUser();
 
-        if (!$entity instanceof BlogPost || Request::METHOD_POST !== $method) {
+        if (!$entity instanceof BlogPost || !$entity instanceof Comment || Request::METHOD_POST !== $method) {
             return;
         }
 
